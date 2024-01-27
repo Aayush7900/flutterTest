56bb388098a1d7091929543b3ef6f50d645a4044
unknown <aayush7900@gmail.com>
2024-01-27 22:58:27 +0545

    images added

--- lib/main.dart
+++ lib/main.dart
@@ -5,98 +5,39 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           centerTitle: true,
           elevation: 5,
-          title: Text('Dice'),
-          backgroundColor: Colors.red.shade900,
+          title: Text('Ask me anything'),
+          backgroundColor: Colors.blue.shade900,
         ),
-        body: DicePage(),
+        body: _8ball(),
       ),
     ),
   );
 }
-//stateless widget
-/*
-class DicePage extends StatelessWidget {
 
-  int leftDiceNumber = 1;
+class _8ball extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    leftDiceNumber = 2;
-
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: TextButton(
-              onPressed: () {},
-              child: Image(
-                image: AssetImage('images/dice$leftDiceNumber.png'),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: TextButton(
-              onPressed: () {
-                print('Right Button Pressed');
-              },
-              child: Image.asset('images/dice2.png'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}*/
-
-class DicePage extends StatefulWidget {
-  @override
-  State<DicePage> createState() => _DicePageState();
+  State<_8ball> createState() => __8ballState();
 }
 
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 2;
-
-  void gen() {
-    leftDiceNumber = Random().nextInt(6) + 1;
-    rightDiceNumber = Random().nextInt(6) + 1;
-  }
+class __8ballState extends State<_8ball> {
+  int ballno = 1;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  gen();
-                });
-              },
-              child: Image(
-                image: AssetImage('images/dice$leftDiceNumber.png'),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  gen();
-                });
-              },
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-            ),
-          ),
-        ],
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballno = 2;
+          });
+        },
+        child: Image.asset(
+          'dice1.png',
+        ),
       ),
     );
   }
